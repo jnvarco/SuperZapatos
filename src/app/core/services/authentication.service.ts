83ff64654a349e25521b66a004158574e587f4c4
Johan Vargas <jnvarco@gmail.com>
2017-11-09 23:54:05 -0600

    jueves en la noche

---
+++ src/app/core/services/authentication.service.ts
@@ -0,0 +1,47 @@
+import { LoggerService } from './../data-services/logger.service';
+import { LocalStorageService } from './local-storage.service';
+import { EventsHubService } from './events-hub.service';
+import { Token } from './../../shared/models/token.model';
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class AuthenticationService {
+
+  private token: Token;
+  private isLoggedIn: boolean;
+
+  constructor(
+    private eventsHubService: EventsHubService,
+    private localStorageService: LocalStorageService,
+    private loggerService: LoggerService
+  ) { 
+    this.isLoggedIn = false;
+
+    this.eventsHubService.isLoggedIn$
+    .subscribe(
+      (isLoggedIn) => {
+        this.isLoggedIn = isLoggedIn;
+      }
+    );    
+  }
+
+  public login(credentials: object): Promise<any>{
+    return new Promise((resolve,reject) => {
+      this.loggerService.login(credentials)
+      .subscribe(
+        (response) => {
+          this.token = <Token>response;
+          this.localStorageService.set('token',this.token);
+
+          resolve(this.token);
+        },
+        (error) => {
+          reject(error);
+        }
+
+      )
+    }
+    );
+  }
+
+}
